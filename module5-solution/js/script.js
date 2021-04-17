01719468a0a82a0cd6d34b4309619b7f7298d6dc
lsantiago123 <lucas.santiago@unifesp.br>
2021-04-17 14:07:49 -0300

    solution5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -85,7 +85,7 @@ $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   function (request) {
     buildAndShowHomeHTML(request);
- },// ***** <---- TODO: STEP 1: Substitute [...] ******
+  },// ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -99,10 +99,9 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
-      insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
-
+       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+       insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
